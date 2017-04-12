506f5562941acfbb4cddc53727d5fe2519729ac0
Isolia <guibertch@gmail.com>
2017-04-12 17:33:44 +0200

    Ajout premiere page

--- app/routes.php
+++ app/routes.php
@@ -2,4 +2,15 @@
 	
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'default_home'],
+		
+		//Authentification
+		['GET|POST', '/login', 'Authen#login', 'login'],
+		['GET|POST', '/register', 'Authen#register', 'register'],
+
+		//HOME
+		['GET', '/home', 'Home#accueil', 'home'],
+
+		['GET', '/membre/profil', 'membres#profil', 'profil'],
+		
+
 	);
